40dd00e14f8d25253695aacff254fb24077ebb41
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-01-05 22:20:41 -0700

    simple working version

--- RecieptScanner/ReceiptScan.py
+++ RecieptScanner/ReceiptScan.py
@@ -9,5 +9,14 @@ res = rq.post(url,
                 data = {'api_key': 'TEST', 'recognizer': 'auto', 'ref_no': 'oct_python_123'},
                 files = {'file': open(receipts, 'rb')})
 
-with open("response1.json", "w") as f:
+with open("output1.json", "w") as f:
     json.dump(json.loads(res.text), f)
+
+try:
+    with open("output1.json", "r") as f:
+        data = json.load(f)
+except FileNotFoundError:
+    print("There seems to be something wrong. No json response file was generated from the input receipts")
+
+# we care about receipts key only
+print(data['receipts'])
