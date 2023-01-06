6e98f9573f3293404d3046a697714ee281f8452d
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-01-05 22:11:47 -0700

    dump works

--- RecieptScanner/ReceiptScan.py
+++ RecieptScanner/ReceiptScan.py
@@ -3,11 +3,11 @@ import json
 import pickle
 
 url = "https://ocr.asprise.com/api/v1/receipt"
-image = "figure1.jpg"
+receipts = "figure1.jpg"
 
 res = rq.post(url, 
                 data = {'api_key': 'TEST', 'recognizer': 'auto', 'ref_no': 'oct_python_123'},
-                files = {'file': open(image, 'rb')})
+                files = {'file': open(receipts, 'rb')})
 
 with open("response1.json", "w") as f:
     json.dump(json.loads(res.text), f)
